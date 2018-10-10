6e2481d57f95dbabee289b84a77706aeaa089739
1mboe <1mboe@Miro-Hauptlaptop>
2018-10-10 21:51:03 +0200

    V 0.2

--- DMSt Rechnner/src/pack1/Main.java
+++ DMSt Rechnner/src/pack1/Main.java
@@ -1,16 +1,39 @@
 package pack1;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.println("Test");
+		int  distanz=0, index=0, punkte=0;
 		
-		if( Jona == 15) {
-			
-			miro = jona + miro;
+		char dreieck = 0;
+		
+		Scanner lesen = new Scanner(System.in);
+		
+		System.out.println("Gebe deine Distanz in Kilometern an: ");
+		//Eingabe des int distanz
+		int anzahl = lesen.nextInt();
+		double [] noten = new double [anzahl];
+		
+		for (int i = 0; i< noten.length;i++){
+			noten[i] = lesen.nextDouble();
+			System.out.print(noten[i] * 100);
 		}
-
+		System.out.println("Gebe deinen Index in als Ganzzahl ein: ");
+		//Eingabe des Index
+		System.out.println("War dein Flug ein FAI-Dreiecks oder ein FAI-Vierecks Flug, so bestätige dies bitte mit einem (J/N)");
+		//Eingabe charachters dreieck
+		
+		
+	    
+		punkte = distanz * 100;
+		punkte = punkte / index;  
+		
+		if( dreieck == 'J') {
+			
+			//+der Punkte für ein Dreieck oder eines Vierecks
+		}
 	}
-
 }
+
